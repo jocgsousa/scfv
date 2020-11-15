c88506f83c1f203dc2287a693fb4ae493da6fce2
Jociel Gregório de Sousa <gregoriociacom@gmail.com>
2020-11-15 11:54:45 -0300

    Model User corrigido

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -7,6 +7,7 @@ class User extends Model {
       name: Sequelize.STRING,
       email: Sequelize.STRING,
       password: Sequelize.VIRTUAL,
+      password_hash: Sequelize.STRING,
       phone: Sequelize.STRING,
       cpf: Sequelize.STRING,
       data_nascimento: Sequelize.STRING,
